fd66caa1c562820d6827f827dbc06f69bff7c650
FacharianHaritsP <43494603+FacharianHaritsP@users.noreply.github.com>
2018-09-22 19:22:31 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117061/latihan15/logika/PBO210117061Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117061.latihan15.logika;
+
+/**
+ *
+ * @author Fachrian Harits Pratama
+ * Nama                 : Fachrian Harits Pratama
+ * Kelas                : PBO2
+ * NIM                  : 10117061
+ * Deskripsi program    : Program ini berisi program operasi boolean dan 
+ *                        operator logika 
+ */
+public class PBO210117061Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       boolean a = true;
+       boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
